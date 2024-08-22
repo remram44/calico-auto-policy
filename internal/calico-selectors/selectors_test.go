629ab55a8c2219976b5dc76047da928bd8e00f67
Remi Rampin <remi@rampin.org>
2024-08-21 22:53:14 -0400

    Add tests for selector conversion

---
+++ internal/calico-selectors/selectors_test.go
@@ -0,0 +1,64 @@
+package calico_selectors
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func mustDecode(jsonString string) map[string]interface{} {
+	var result map[string]interface{}
+	err := json.Unmarshal([]byte(jsonString), &result)
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
+
+func translate(jsonString string, t *testing.T) string {
+	calicoSelector, err := KubernetesToCalicoNetworkPolicySelectors(mustDecode(jsonString))
+	if err != nil {
+		t.Fatal(err)
+	}
+	return calicoSelector
+}
+
+func testTranslate(jsonString string, expectedCalico string, t *testing.T) {
+	result := translate(jsonString, t)
+	if result != expectedCalico {
+		t.Errorf("%#v != %#v", expectedCalico, result)
+	}
+}
+
+func TestLabels(t *testing.T) {
+	testTranslate(
+		"{\"matchLabels\": {\"app\": \"email\"}}",
+		"app == 'email'",
+		t,
+	)
+	testTranslate(
+		"{\"matchLabels\": {\"app\": \"email\", \"tier\": \"prod\"}}",
+		"app == 'email' && tier == 'prod'",
+		t,
+	)
+}
+
+func TestExpressions(t *testing.T) {
+	testTranslate(
+		"{\"matchExpressions\": [{\"key\": \"app\", \"operator\": \"In\", \"values\": [\"email\", \"chat\"]}, {\"key\": \"tier\", \"operator\": \"Exists\"}]}",
+		"app in {'email', 'chat'} && has(tier)",
+		t,
+	)
+	testTranslate(
+		"{\"matchExpressions\": [{\"key\": \"app\", \"operator\": \"NotIn\", \"values\": [\"chat\"]}]}",
+		"app not in {'chat'}",
+		t,
+	)
+}
+
+func TestBoth(t *testing.T) {
+	testTranslate(
+		"{\"matchLabels\": {\"tier\": \"prod\"}, \"matchExpressions\": [{\"key\": \"app\", \"operator\": \"NotIn\", \"values\": [\"chat\"]}]}",
+		"tier == 'prod' && app not in {'chat'}",
+		t,
+	)
+}
